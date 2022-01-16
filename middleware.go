c22e674ee47e0cbbaad87cc65b07f6f8588b0247
NastyPigz <williamzwy06701@gmail.com>
2022-01-15 23:11:32 -0500

    Optimised

--- middleware.go
+++ middleware.go
@@ -6,21 +6,19 @@ import (
 )
 
 func AddHandleFunc(route string, content string, f func(http.ResponseWriter, *http.Request)) {
-	pages = append(pages, page{route: route, content: content, f: f})
+	pages[route] = page{route: route, content: content, f: f}
 }
 
 func URLHandler(w http.ResponseWriter, req *http.Request) {
 	title := req.URL.Path
-	for _, t := range pages {
-		if t.route == title || t.route[1:] == title {
-			if t.content == "" {
-				t.content = "text/plain"
-			}
-			w.Header().Set("Content-Type", t.content)
-			w.Header().Set("Access-Control-Allow-Origin", "*")
-			t.f(w, req)
-			return
+	if t, v := pages[title]; v {
+		if t.content == "" {
+			t.content = "text/plain"
 		}
+		w.Header().Set("Content-Type", t.content)
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		t.f(w, req)
+		return
 	}
 	Utils.Include("404.html", w, nil)
 }
@@ -31,4 +29,4 @@ type page struct {
 	f       func(http.ResponseWriter, *http.Request)
 }
 
-var pages []page = []page{}
+var pages map[string]page = map[string]page{}
